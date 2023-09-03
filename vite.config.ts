f6ba7a2822a72515492b14d04b11e59f535d52d0
Alex <grashupfer99@gmail.com>
2023-09-03 14:50:47 +0900

    basic config, lint, ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), viteTsConfigPaths()],
+  server: {
+    host: "0.0.0.0",
+    port: 8000,
+  },
+});
