280de65f9ff1045b4cfbfca73ca80fd9c65ead7c
Alex <grashupfer99@gmail.com>
2023-09-03 15:24:22 +0900

    add rest client, link interfaces

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,38 @@
-// add interfaces here
+interface IFile {
+  /** File's KEY */
+  key: string;
+  /** File's thumbnail URL */
+  thumbnailUrl: string;
+  /**  File's name  */
+  name: string;
+  /** File's size  */
+  size: number;
+}
+
+interface ILink {
+  /** Date when the link was created */
+  created_at: number;
+  /** Link's KEY */
+  key: string | undefined;
+  /** Link's validity period */
+  expires_at: number;
+  /** Number of times the link has been downloaded */
+  download_count: number;
+  /** Number of files in the link */
+  count: number;
+  /**  File size of the link */
+  size: number;
+  /** Name of the first file in the link */
+  summary: string;
+  /**  Thumbnail URL of the link */
+  thumbnailUrl: string;
+  files: IFile[];
+  sent: {
+    /** Email subject when sharing the link */
+    subject: string;
+    /** Email content when sharing the link */
+    content: string;
+    /** List of emails the link was shared with */
+    emails: string[];
+  };
+}
